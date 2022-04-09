034655fa6a74def96f266435c770cf2f5250bcc0
王伟 <872937094@qq.com>
2022-04-10 00:54:01 +0800

    引入window.self来获取到render函数中的this方便调试

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,9 +1,10 @@
 import {h} from '../../lib/guide-mini-vue.esm.js'
+window.self = null
 export const App = {
     // .vue
     // 我们还未实现template的编译功能 我们先去实现我们的render函数
     render(){
-
+        window.self = this
 
         return h('div',{ 
             id:"root",
