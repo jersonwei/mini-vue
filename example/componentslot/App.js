4d014d617266128b9d6c76154f11c8d9b6e6d830
王伟 <872937094@qq.com>
2022-04-10 21:37:18 +0800

    完成header-footer初步指定位置的渲染

--- example/componentslot/App.js
+++ example/componentslot/App.js
@@ -4,7 +4,13 @@ export const App = {
     name:"App",
     render(){
         const app = h('div',{},"App")
-        const foo = h(Foo,{},[h("p",{},"123"),h('p',{},'234')])
+        // 接受两个数据形式 数组和单个
+        // 把数组的形式换成对象的key模式
+        // const foo = h(Foo,{},[h("p",{},"123"),h('p',{},'234')])
+        const foo = h(Foo,{},{
+            header:h("p",{},"header"),
+            footer:h('p',{},'footer')
+        })
         return h('div',{},[app,foo])},
     setup(){
         return {
--- example/componentslot/foo.js
+++ example/componentslot/foo.js
@@ -11,6 +11,7 @@ export const Foo ={
         // 我们需要将内部的东西转换成虚拟节点
 
         // 将下面的逻辑封装成函数  帮助我们渲染我们的slots
-        return h('div',{},[foo,renderSlots(this.$slots)])
+        // 添加渲染指定位置的逻辑
+        return h('div',{},[renderSlots(this.$slots,"header"),foo,renderSlots(this.$slots,"footer")])
     }
 }
