23ca7195830d07eec89a9de1ff78add2df741b3e
王伟 <872937094@qq.com>
2022-04-10 15:09:01 +0800

    完成foo组件中读取setup中this指向的问题

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -5,11 +5,17 @@ const publicPropertiesMap = {
 export const PublicInstanceProxyHandlers = {
     get({_:instance},key){
         // 先从setupstate中获取值
-        const {setupState} = instance
-        if(key in setupState){
-            return setupState[key]
-        }
-
+        const {setupState,props} = instance
+        // if(key in setupState){
+        //     return setupState[key]
+        // }
+        // 将上面的逻辑进行重构 加上我们的props的逻辑
+        const hasOwn = (val,key)=> Object.prototype.hasOwnProperty.call(val,key)
+            if(hasOwn(setupState,key)){
+                return setupState[key]
+            }else if(hasOwn(props,key)){
+                return props[key]
+            }
         // key=>el
         const publicGetter = publicPropertiesMap[key]
         if(publicGetter){
