9830ffee1ce1d246a5a0c3c506aec153022d1f3a
王伟 <872937094@qq.com>
2022-04-10 14:58:25 +0800

    将instance.props注入到我们的setup中就接受到了我们的count

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -30,7 +30,7 @@ function setupStatefulComponent (instance:any){
         // 我们的setup可以返回一个对象或者是函数
         // 当我们返回一个函数时 就可以把它认为是我们的render函数
         // 如果返回的是一个对象 会把这个对象注入到我们组件的上下文中
-        const setupResult = setup()
+        const setupResult = setup(instance.props)
 
         handleSetupResult(instance,setupResult)
     }
