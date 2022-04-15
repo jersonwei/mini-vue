7bc48255c4b668fc0a33cb39def8aa0865f39370
王伟 <872937094@qq.com>
2022-04-16 01:42:50 +0800

    diff双端对比乱序部分查找是否有key及删除新值中没有的老值

--- example/patchChildren/ArrayToAarray.js
+++ example/patchChildren/ArrayToAarray.js
@@ -49,12 +49,12 @@ import {ref,h} from '../../lib/guide-mini-vue.esm.js'
 // (a,b)
 // c (a,b)
 // i=0 e1 = -1 e2 = 0
-const prevChildren = [h('p',{key:'A'},'A'),h('p',{key:'B'},'B')]
-const nextChildren = [
-        h('p',{key:'D'},'D'),
-        h('p',{key:'C'},'C'),
-        h('p',{key:'A'},'A'),
-        h('p',{key:'B'},'B')]
+// const prevChildren = [h('p',{key:'A'},'A'),h('p',{key:'B'},'B')]
+// const nextChildren = [
+//         h('p',{key:'D'},'D'),
+//         h('p',{key:'C'},'C'),
+//         h('p',{key:'A'},'A'),
+//         h('p',{key:'B'},'B')]
 
 // 4 老的比新的长 右侧
 // 删除老的
@@ -73,6 +73,39 @@ const nextChildren = [
 // const nextChildren = [
 //         h('p',{key:'C'},'C'),
 //         h('p',{key:'D'},'D')]
+
+// 5对比中间的部分
+// 删除老的 (在老的里面存在,新的里面不存在)
+// 5.1
+// a,b(c,d),f,g
+// a,b(e,c),f,g
+// D 节点在新的里面没有的 需要删除
+// C 节点在props也发生了变化
+
+const prevChildren = [h('p',{key:'A'},'A'),
+h('p',{key:'B'},'B'),
+h('p',{key:'C',id:'c-prev'},'C'),
+h('p',{key:'D'},'D'),
+h('p',{key:'F'},'F'),
+h('p',{key:'G'},'G')]
+const nextChildren = [h('p',{key:'A'},'A'),
+h('p',{key:'B'},'B'),
+h('p',{key:'E'},'E'),
+h('p',{key:'C',id:'c-next'},'C'),
+h('p',{key:'F'},'F'),
+h('p',{key:'G'},'G')]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
