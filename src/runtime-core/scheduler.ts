f322e40ae5ec96ce920b420d3735b5d9570642ec
王伟 <872937094@qq.com>
2022-04-17 21:00:41 +0800

    实现nextTick函数的逻辑及抽离上一步的promise代码

--- src/runtime-core/scheduler.ts
+++ src/runtime-core/scheduler.ts
@@ -1,6 +1,13 @@
 const queue:any[] = []
-export function queueJobs(job){
+// 引入一个开关
+let isFlushPending = false
 
+export function nextTick(fn){
+
+    return fn? Promise.resolve().then(fn) : Promise.resolve()
+}
+
+export function queueJobs(job){
 
     if(queue.includes(job)){
 
@@ -11,10 +18,24 @@ export function queueJobs(job){
 }
 
 function queueFlush(){
-    Promise.resolve().then(()=>{
-        let job 
-        while (job = queue.shift()) {
-            job && job()
-        }
-    })
+    if(isFlushPending) return
+    isFlushPending = true
+
+    nextTick(flushJobs)
+    // Promise.resolve().then(()=>{
+        // 这段代码可以利用上面的nextTick来执行  将下面的代码进行抽离
+    // })
 }
+
+function flushJobs(){
+    isFlushPending = false  // 重置开关
+    let job 
+    while (job = queue.shift()) {
+        job && job()
+    }
+}
+
+
+
+
+
