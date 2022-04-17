5c5adff2363d8313e895989e6d162aabe3b866fd
王伟 <872937094@qq.com>
2022-04-17 22:58:33 +0800

    将我们的content进行逻辑的细化拆分

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -3,18 +3,27 @@
 export function baseParse(content:string){
     // 创建上下文对象
     const context = createParseContext(content)
-        return {
-            children:[
-                {
-                    type:'interpolation',
-                    content:{
-                        type:'simple_expression',
-                        content:'message'
-                    }
-                }
-            ]
-        }
+    return createRoot(parseChildren(context))    
 }
+
+function parseChildren(context){
+    const nodes:any = []
+    const node = parseInterpolation()
+    nodes.push(node)
+
+    return nodes
+}
+
+function parseInterpolation(){
+    return {
+        type:'interpolation',
+            content:{
+                type:'simple_expression',
+                content:'message'
+            }
+    }
+}
+
 
 function createRoot(children){
     return {
