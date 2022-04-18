f89e2a4474e5f38cf2ef797f3acd63b451e678a4
王伟 <872937094@qq.com>
2022-04-19 01:40:58 +0800

    用switch语句细化不同类型的编译逻辑过程

--- src/compiler-core/src/codegen.ts
+++ src/compiler-core/src/codegen.ts
@@ -1,3 +1,4 @@
+import { NodeTypes } from "./ast"
 
 export function generate(ast){
     const context = createCodegenContext()
@@ -17,7 +18,7 @@ export function generate(ast){
     const args = ['_ctx','_cache']
     const signature = args.join(', ')
     // const node = ast.codegenNode
-
+    console.log(ast)
     push(`function ${functionName}(${signature}){`)
     // code += `function ${functionName}(${signature}){`
     push(`return`)
@@ -35,7 +36,9 @@ function genFunctionPreamble(ast,context){
     const VueBinging = "Vue"
     // const helpers = ["toDisplayString"]
     const aliasHelper = (s)=>`${s}:_${s}`;
-    push(`const { ${ast.helpers.map(aliasHelper).join(', ')} } = ${VueBinging}`)
+    if(ast.helpers.length>0){
+        push(`const { ${ast.helpers.map(aliasHelper).join(', ')} } = ${VueBinging}`)
+    }
     push('\n')
     push('return ')
 }
@@ -51,13 +54,43 @@ function createCodegenContext():any{
 }
 
 function genNode(node:any,context){
-    const {push} = context
-    // const node = ast.codegenNode
-    push(`'${node.content}'`)
-    // code += `return '${node.content}'`
-    // return code
+    switch (node.type) {
+        case NodeTypes.TEXT:
+            genText(node,context)
+            // Text类型
+            // const {push} = context
+            // const node = ast.codegenNode
+            // push(`'${node.content}'`)
+            // code += `return '${node.content}'`
+            // return code
+            break;
+        case NodeTypes.INTERPOLATION:
+
+            geninterpolation(node,context)
+            break;
+        case NodeTypes.SIMPLE_EXPRESSION:
+            genExpression(node,context)
+        default:
+            break;
+    }
+
 }
 
+function genExpression(node:any,context:any){
+    const {push} = context
 
+    push(`_ctx.${node.content}`)
+}
 
+function genText(node:any,context:any){
+    const {push} = context
+    push(`'${node.content}'`)
+}
+
+function geninterpolation(node:any,context:any){
+    const {push} = context
+    push(`_toDisplayString(`)
+    genNode(node.content,context)
+    push(")")
+}
 
--- src/compiler-core/tests/codegen.spec.ts
+++ src/compiler-core/tests/codegen.spec.ts
@@ -6,14 +6,14 @@ import { transform } from "../src/transform"
 describe('codegen',()=>{
 
 
+    // 快照测试 对我们当前的code进行一次快照 当我们代码或逻辑改动 前后两次的快照不一样就会报错
+    // 1.抓BUG
+    // 2.有意更新
     it('string',()=>{
 
     const ast = baseParse('hi')
     transform(ast)
     const {code} = generate(ast)
-    // 快照测试 对我们当前的code进行一次快照 当我们代码或逻辑改动 前后两次的快照不一样就会报错
-    // 1.抓BUG
-    // 2.有意更新
     expect(code).toMatchSnapshot()
     })
 
