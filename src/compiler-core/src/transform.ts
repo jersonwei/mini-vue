83ac4962f2cbaaec71b9d77cd2ed6794425b430f
王伟 <872937094@qq.com>
2022-04-18 23:06:52 +0800

    使用设计模式中的插件思想,将动态逻辑抽离到外部执行内部只需调用

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,25 +1,40 @@
 import { NodeTypes } from "./ast"
 
-export function transform(root){
-
+export function transform(root,options){
+    // 存储我们的初始值
+    const context = createTransformContext(root,options)
     // 1 遍历 深度优先搜索
-    traverseNode(root)
+    traverseNode(root,context)
 
     // 2 修改 text content
 
 }
-
-function traverseNode(node:any){
-    console.log(node)
-    if(node.type === NodeTypes.TEXT){
-        node.content = node.content + ' mini-vue'
+function createTransformContext(root:any,options:any):any{
+    const context = {
+        root,
+        nodeTransforms:options.nodeTransforms || []
     }
+
+    return context
+}
+
+
+function traverseNode(node:any,context){
+    console.log(node)
+    // if(node.type === NodeTypes.TEXT){
+    //     node.content = node.content + ' mini-vue'
+    // }
+    const nodeTransforms = context.nodeTransforms
+   for (let i = 0; i < nodeTransforms.length; i++) {
+        const transform = nodeTransforms[i]
+        transform(node)
+   }
     const children = node.children
 
     if(children){
        for (let i = 0; i < children.length; i++) {
             const node = children[i]
-            traverseNode(node)
+            traverseNode(node,context)
        }
     }
 }
